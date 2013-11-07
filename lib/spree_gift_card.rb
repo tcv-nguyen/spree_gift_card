51795a500740f7946acf23a0acbad0ad49b6fc90
Tony Nguyen <tony_nlc@yahoo.com>
2013-11-07 01:09:44 -0600

    refs #400: Create public method to get Durably SHA

--- lib/spree_gift_card.rb
+++ lib/spree_gift_card.rb
@@ -4,3 +4,10 @@ require 'spree_backend'
 require 'spree_core'
 require 'spree_frontend'
 require 'spree_gift_card/engine'
+
+# Create public method to get durably_sha
+public
+  
+  def get_durably_sha( method )
+    DurableDecorator::Base.determine_sha method
+  end
